b25150fd5089ae0aa8c5b47285023f72c81220e8
Burak Altas <burak.altas@data-boss.com.tr>
2021-04-02 17:31:19 +0300

    Adjustments

--- src/components/dnd/topbar.tsx
+++ src/components/dnd/topbar.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
-import SubmitRequestUtils from "../event/utils";
+import RequestUtils from "../event/utils";
 
 const TopBar = () => {
     return (
         <div>
-            <button onClick={() => SubmitRequestUtils.submit("formData")} className="topbarbutton">Submit</button>
-            <button onClick={() => SubmitRequestUtils.submit("formData")} className="topbarbutton">Retry</button>
-            <button onClick={() => SubmitRequestUtils.submit("formData")} className="topbarbutton">Resubmit</button>
-            <button onClick={() => SubmitRequestUtils.submit("formData")} className="topbarbutton">Suspend</button>
-            <button onClick={() => SubmitRequestUtils.submit("formData")} className="topbarbutton">Resume</button>
-            <button onClick={() => SubmitRequestUtils.submit("formData")} className="topbarbutton">Stop</button>
-            <button onClick={() => SubmitRequestUtils.submit("formData")} className="topbarbutton">Terminate</button>
-            <button onClick={() => SubmitRequestUtils.submit("formData")} className="topbarbutton">Delete</button>
+            <button onClick={() => RequestUtils.submit("formData")} className="topbarbutton">Submit</button>
+            <button onClick={() => RequestUtils.resubmit("formData")} className="topbarbutton">Resubmit</button>
+            <button onClick={() => RequestUtils.suspend("formData")} className="topbarbutton">Suspend</button>
+            <button onClick={() => RequestUtils.resume("formData")} className="topbarbutton">Resume</button>
+            <button onClick={() => RequestUtils.stop("formData")} className="topbarbutton">Stop</button>
+            <button onClick={() => RequestUtils.terminate("formData")} className="topbarbutton">Terminate</button>
+            <button onClick={() => RequestUtils.delete("formData")} className="topbarbutton">Delete</button>
         </div>
 
     );
--- src/components/event/utils.tsx
+++ src/components/event/utils.tsx
@@ -1,19 +1,89 @@
 import axios from "axios";
 
-const SUBMIT_API=process.env.SUBMIT_API || "https://localhost:5000/submit";
+const SUBMIT_API=process.env.SUBMIT_API || "https://localhost:5000/";
 
-export default class SubmitRequestUtils {
+export default class RequestUtils {
     static submit(param:string) {
-        console.log(param);
+        console.log("Submit initiated.");
         const newAxios=axios.create({
             headers: {
                 'cache-control': 'no-cache',
-                'Content-Type': 'multipart/form-data',
-                'Auth': ` eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImJpZ19kYXRhIiwicGFzc3dvcmQiOiJiaWdfZGF0YSJ9.xIa0Qn9ITJjSTW2NeiM3abKeIWzrlF3nl9Wt_qKm0d0`
+                'Content-Type': 'application/json',
             }
         })
         newAxios.post(SUBMIT_API,param,
-            {}).then((response) => console.log(response)).
-        catch((error) => console.log(error));
+            {}).then((response) => console.log(response)).catch((error) => console.log(error));
+    }
+
+    static resubmit(param:string) {
+        console.log("Resubmit initiated.");
+        const newAxios=axios.create({
+            headers: {
+                'cache-control': 'no-cache',
+                'Content-Type': 'application/json',
+            }
+        })
+        newAxios.post(SUBMIT_API,param,
+            {}).then((response) => console.log(response)).catch((error) => console.log(error));
+    }
+
+    static suspend(param:string) {
+        console.log("Suspend initiated.");
+        const newAxios=axios.create({
+            headers: {
+                'cache-control': 'no-cache',
+                'Content-Type': 'application/json',
+            }
+        })
+        newAxios.post(SUBMIT_API,param,
+            {}).then((response) => console.log(response)).catch((error) => console.log(error));
+    }
+
+    static resume(param:string) {
+        console.log("Resume initiated.");
+        const newAxios=axios.create({
+            headers: {
+                'cache-control': 'no-cache',
+                'Content-Type': 'application/json',
+            }
+        })
+        newAxios.post(SUBMIT_API,param,
+            {}).then((response) => console.log(response)).catch((error) => console.log(error));
+    }
+
+    static stop(param:string) {
+        console.log("Stop initiated.");
+        const newAxios=axios.create({
+            headers: {
+                'cache-control': 'no-cache',
+                'Content-Type': 'application/json',
+            }
+        })
+        newAxios.post(SUBMIT_API,param,
+            {}).then((response) => console.log(response)).catch((error) => console.log(error));
+    }
+
+    static terminate(param:string) {
+        console.log("Terminate initiated.");
+        const newAxios=axios.create({
+            headers: {
+                'cache-control': 'no-cache',
+                'Content-Type': 'application/json',
+            }
+        })
+        newAxios.post(SUBMIT_API,param,
+            {}).then((response) => console.log(response)).catch((error) => console.log(error));
+    }
+
+    static delete(param:string) {
+        console.log("Delete initiated.");
+        const newAxios=axios.create({
+            headers: {
+                'cache-control': 'no-cache',
+                'Content-Type': 'application/json',
+            }
+        })
+        newAxios.post(SUBMIT_API,param,
+            {}).then((response) => console.log(response)).catch((error) => console.log(error));
     }
 }
