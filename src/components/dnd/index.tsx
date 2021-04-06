b685f8b5585d123c5fb72054b1ac6216b779d344
altas <burak.altas@gmail.com>
2021-04-06 17:27:56 +0300

    Buttons are functional

--- src/components/dnd/index.tsx
+++ src/components/dnd/index.tsx
@@ -18,25 +18,84 @@ import './dnd.css';
 import S3 from "../nodes/S3";
 import KAFKA from "../nodes/KAFKA";
 import ELASTICSEARCH from "../nodes/ELASTICSEARCH";
+import {Task} from "../data/interface";
+import State from "../data/state";
 
-const initialElements: Elements | (() => Elements) = [];
+const Read = "Read";
+const Write = "Write";
+
+const initialNodes: Elements | (() => Elements) = [];
+const initialEdges: Elements | (() => Elements) = [];
+
+let counter:number = 0;
 
 const onDragOver = (event: DragEvent) => {
     event.preventDefault();
     event.dataTransfer.dropEffect = 'move';
 };
 
+
+function createTasksForEdge(edge: Edge, edges:Elements) : void {
+    const dep = hasDependency(edge.source, edges);
+    const dependencies:Array<string> = [];
+    if (dep) {
+        dependencies.push(edge.source + Write);
+    }
+    let task : Task = {
+        name: edge.source + Read,
+        dependencies: dependencies,
+        templateRef: {
+            "name": "orca-operators",
+            "template": "orca-operators"
+        },
+        arguments: {
+            parameters: [{"name": "OPERATOR", "value": "s3"}, {"name": "OPERATOR_TYPE", "value": "read"},
+                {"name": "REDIS_URL", "value": "192.168.2.101"}, {"name": "REDIS_PORT", "value": "6379"}, {"name": "REDIS_PUSH_KEY", "value": "testFinal"}, {"name": "REDIS_POP_KEY", "value": "None"},
+                {"name": "AWS_S3_BUCKET_NAME", "value": "databoss-weather-test"}, {"name": "AWS_S3_FILE_PATH", "value": "weather.csv"}, {"name": "AWS_S3_FILE_TYPE", "value": "CSV"},
+                {"name": "BOOTSTRAP_SERVERS", "value": "192.168.2.102:9094"}, {"name": "KAFKA_TOPIC", "value": "testFinal"},
+                {"name": "ELASTICSEARCH_HOST", "value": "http://192.168.2.101:9200"},  {"name": "ELASTICSEARCH_INDEX", "value": "test"}]
+        }
+    };
+    State.tasks.push(task);
+    task = {
+        name: edge.target + Write,
+        dependencies: [edge.source + Read],
+        templateRef: {
+            "name": "orca-operators",
+            "template": "orca-operators"
+        },
+        arguments: {
+            parameters: [{"name": "OPERATOR", "value": "s3"}, {"name": "OPERATOR_TYPE", "value": "read"},
+                {"name": "REDIS_URL", "value": "192.168.2.101"}, {"name": "REDIS_PORT", "value": "6379"}, {"name": "REDIS_PUSH_KEY", "value": "testFinal"}, {"name": "REDIS_POP_KEY", "value": "None"},
+                {"name": "AWS_S3_BUCKET_NAME", "value": "databoss-weather-test"}, {"name": "AWS_S3_FILE_PATH", "value": "weather.csv"}, {"name": "AWS_S3_FILE_TYPE", "value": "CSV"},
+                {"name": "BOOTSTRAP_SERVERS", "value": "192.168.2.102:9094"}, {"name": "KAFKA_TOPIC", "value": "testFinal"},
+                {"name": "ELASTICSEARCH_HOST", "value": "http://192.168.2.101:9200"},  {"name": "ELASTICSEARCH_INDEX", "value": "test"}]
+        }
+    };
+    State.tasks.push(task);
+    console.log(State.tasks);
+}
+
+function hasDependency(nodeName:string, edges:Elements):boolean {
+    const edge = edges.find(x => (x as Edge).target === nodeName);
+    return edge !== undefined;
+}
+
 const DnDFlow = () => {
     const [reactFlowInstance, setReactFlowInstance] = useState<OnLoadParams>();
-    const [elements, setElements] = useState<Elements>(initialElements);
+    const [nodes, setNodes] = useState<Elements>(initialNodes);
+    const [edges, setEdges] = useState<Elements>(initialEdges);
+
 
     const onConnect = (params: Edge | Connection) => {
         (params as Edge).animated = true;
         (params as Edge).arrowHeadType = ArrowHeadType.ArrowClosed;
-        setElements((elements) => addEdge(params, elements));
+        setEdges((edges) => addEdge(params, edges));
+        createTasksForEdge(params as Edge, edges);
     }
-    console.log(elements);
-    const onElementsRemove = (elementsToRemove: Elements) => setElements((elements) => removeElements(elementsToRemove, elements));
+    console.log(nodes);
+    console.log(edges);
+    const onElementsRemove = (elementsToRemove: Elements) => setNodes((nodes) => removeElements(elementsToRemove, nodes));
     const onLoad = (_reactFlowInstance: OnLoadParams) => setReactFlowInstance(_reactFlowInstance);
 
     const onDrop = (event: DragEvent) => {
@@ -51,8 +110,8 @@ const DnDFlow = () => {
                 position,
                 data: { label: `${type}` },
             };
-
-            setElements((es) => es.concat(newNode));
+            counter++;
+            setNodes((es) => es.concat(newNode));
         }
     };
 
@@ -69,7 +128,7 @@ const DnDFlow = () => {
                 <div className="reactflow-wrapper">
                     <TopBar/>
                     <ReactFlow
-                        elements={elements}
+                        elements={nodes.concat(edges)}
                         onConnect={onConnect}
                         onElementsRemove={onElementsRemove}
                         onLoad={onLoad}
--- src/components/dnd/topbar.tsx
+++ src/components/dnd/topbar.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-
+import {Workflow} from "../data/interface";
 import RequestUtils from "../event/utils";
+import State from "../data/state";
 
 const TopBar = () => {
     return (
         <div>
-            <button onClick={() => RequestUtils.submit("formData")} className="topbarbutton">Submit</button>
-            <button onClick={() => RequestUtils.resubmit("formData")} className="topbarbutton">Resubmit</button>
-            <button onClick={() => RequestUtils.suspend("formData")} className="topbarbutton">Suspend</button>
-            <button onClick={() => RequestUtils.resume("formData")} className="topbarbutton">Resume</button>
-            <button onClick={() => RequestUtils.stop("formData")} className="topbarbutton">Stop</button>
-            <button onClick={() => RequestUtils.terminate("formData")} className="topbarbutton">Terminate</button>
-            <button onClick={() => RequestUtils.delete("formData")} className="topbarbutton">Delete</button>
+            <button onClick={() => RequestUtils.submit(new class implements Workflow {
+                name= "test-";
+                tasks = State.tasks;
+            })} className="topbarbutton">Submit</button>
+            <button onClick={() => RequestUtils.resubmit()} className="topbarbutton">Resubmit</button>
+            <button onClick={() => RequestUtils.suspend()} className="topbarbutton">Suspend</button>
+            <button onClick={() => RequestUtils.resume()} className="topbarbutton">Resume</button>
+            <button onClick={() => RequestUtils.stop()} className="topbarbutton">Stop</button>
+            <button onClick={() => RequestUtils.terminate()} className="topbarbutton">Terminate</button>
+            <button onClick={() => RequestUtils.delete()} className="topbarbutton">Delete</button>
         </div>
 
     );
