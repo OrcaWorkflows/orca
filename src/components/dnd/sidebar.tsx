68e72666cb4dd4bfcb1b3e163d571abb849f3e28
BugraOzturk <bugra.ozturk@data-boss.com.tr>
2021-04-11 12:29:57 +0300

    SideBar Row Component Implemented

--- src/components/dnd/sidebar.tsx
+++ src/components/dnd/sidebar.tsx
@@ -1,61 +1,40 @@
 import React from 'react';
-import { Table } from "react-bootstrap";
+import {Table} from "react-bootstrap";
 import Collapsible from "react-collapsible";
+import NodeRow from "./rowgenerator";
 
 const sidebar = () => {
-    const onDragStart = (event: React.DragEvent<HTMLDivElement>, nodeType: string) => {
-        event.dataTransfer.setData('application/reactflow', nodeType);
-        event.dataTransfer.effectAllowed = 'move';
-    };
-
     return (
         <div className="sidebar">
             <Table className={"sidebar-table"}>
                 <tbody>
-                    <tr>
-                        <Collapsible trigger="Amazon Web Service" open={true}>
-                            <Table className={"sidebar-table"}>
-                                <tr>
-                                    <td>
-                                        <div className="s3" onDragStart={(event) => onDragStart(event, 'S3')}
-                                             draggable/>
-                                    </td>
-                                    <td>
-                                        <span>S3</span>
-                                    </td>
-                                </tr>
-                            </Table>
-                        </Collapsible>
-                    </tr>
-                    <tr>
-                        <Collapsible trigger={"Apache Stack"} open={true}>
-                            <Table className={"sidebar-table"}>
-                                <tr>
-                                    <td>
-                                        <div className="kafka" onDragStart={(event) => onDragStart(event, 'Kafka')} draggable/>
-                                    </td>
-                                    <td>
-                                        <span>Kafka</span>
-                                    </td>
-                                </tr>
-                            </Table>
-                        </Collapsible>
-                    </tr>
-                    <tr>
-                        <Collapsible trigger={"ELK Stack"} open={true}>
-                            <Table className={"sidebar-table"}>
-                                <tr>
-                                    <td>
-                                        <div className="elasticsearch" onDragStart={(event) => onDragStart(event, 'Elasticsearch')}
-                                             draggable/>
-                                    </td>
-                                    <td>
-                                        <span>Elasticsearch</span>
-                                    </td>
-                                </tr>
-                            </Table>
-                        </Collapsible>
-                    </tr>
+                <tr>
+                    <Collapsible trigger="Amazon Web Service" open={true}>
+                        <Table className={"sidebar-table"}>
+                            <tbody>
+                                <NodeRow node={"S3"}/>
+                            </tbody>
+                        </Table>
+                    </Collapsible>
+                </tr>
+                <tr>
+                    <Collapsible trigger={"Apache Stack"} open={true}>
+                        <Table className={"sidebar-table"}>
+                            <tbody>
+                                <NodeRow node={"Kafka"}/>
+                            </tbody>
+                        </Table>
+                    </Collapsible>
+                </tr>
+                <tr>
+                    <Collapsible trigger={"ELK Stack"} open={true}>
+                        <Table className={"sidebar-table"}>
+                            <tbody>
+                                <NodeRow node={"Elasticsearch"}/>
+                            </tbody>
+                        </Table>
+                    </Collapsible>
+                </tr>
                 </tbody>
             </Table>
         </div>
